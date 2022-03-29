c2a4f240d1e9bd4af1c6d0c0f2993780471e7513
ArtemDutchak <artemdutchak@gmail.com>
2022-03-29 09:54:15 +0300

    add empty page component

---
+++ resources/js/components/pages/Categories.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <v-container>
+
+    <v-row>
+      <v-col class="text-center">
+        <span>
+          {{ title }}
+        </span>
+      </v-col>
+    </v-row>
+
+  </v-container>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Categories',
+
+  data: () => ({
+    title: "Categories",
+  }),
+
+  methods:{
+
+  },
+
+  computed:{
+
+  },
+
+  mounted() {
+
+  }
+
+}
+</script>
