8e8d4d45d9ac2493bdbc36199143ec116c184f40
ArtemDutchak <artemdutchak@gmail.com>
2022-03-29 09:54:38 +0300

    add empty page component

---
+++ resources/js/components/pages/Favorites.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <v-container>
+
+    <v-row>
+      <v-col class="text-center">
+        <span>
+          {{ title }}
+        </span>
+      </v-col>
+    </v-row>
+
+  </v-container>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Favorites',
+
+  data: () => ({
+    title: "Favorites",
+  }),
+
+  methods:{
+
+  },
+
+  computed:{
+
+  },
+
+  mounted() {
+
+  }
+
+}
+</script>
