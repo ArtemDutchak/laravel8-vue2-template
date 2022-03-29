b6bcd9bf547137baabcfd69bb439bd4ce4caeb85
ArtemDutchak <artemdutchak@gmail.com>
2022-03-29 09:58:30 +0300

    change routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -5,6 +5,15 @@ import {i18n} from '../i18n'
 import Layout from '../components/layouts/Layout';
 
 import Home from '../components/pages/Home';
+
+import Dashboard from '../components/pages/Dashboard';
+import Categories from '../components/pages/Categories';
+import CustomUrls from '../components/pages/CustomUrls';
+import ScanList from '../components/pages/ScanList';
+import Favorites from '../components/pages/Favorites';
+import Listing from '../components/pages/Listing';
+import Settings from '../components/pages/Settings';
+
 import E404 from '../components/pages/E404';
 
 const routes = [
@@ -14,23 +23,17 @@ const routes = [
       render(c){return c('router-view')}
     },
     children: [
-      { path: '/', name: 'Home', component: Home },
-      { path: 'e404', name: 'E404', component: E404 },
-      { path: 'login', name: 'Login', component: Home },
-      { path: 'error', name: 'Error', component: Home },
-      { path: 'app', name: 'Layout', component: Home, children: [
-        { path: 'dashboard', name: 'AnalyticsPage', component: Home },
-        { path: 'settings', name: 'SettingsPage', component: Home },
-        { path: 'operations', name: 'OperationsPage'},
-        { path: 'operations/product-update', name: 'ProductUpdatePage', component: Home },
-        { path: 'operations/export', name: 'ExportPage', component: Home },
-        { path: 'typography', name: 'TypographyPage', component: Home },
-        { path: 'components', name: 'Components'},
-        { path: 'components/icons', name: 'IconsPage', component: Home },
-        { path: 'notifications', name: 'NotificationsPage', component: Home },
-        { path: 'components/charts', name: 'ChartsPage', component: Home },
-        { path: 'tables', name: 'TablesBasicPage', component: Home },
-        { path: 'components/maps', name: 'GoogleMapPage', component: Home },
+      { path: '/', name: 'HomePage', component: Home },
+      { path: 'e404', name: 'E404Page', component: E404 },
+      { path: 'error', name: 'ErrorPage', component: Home },
+      { path: '', name: 'Layout', component: Layout, children: [
+        { path: 'dashboard', name: 'DashboardPage', component: Dashboard },
+        { path: 'categories', name: 'CategoriesPage', component: Categories },
+        { path: 'custom-urls', name: 'CustomUrlsPage', component: CustomUrls },
+        { path: 'scan-list', name: 'ScanListPage', component: ScanList },
+        { path: 'favorites', name: 'FavoritesPage', component: Favorites },
+        { path: 'listing', name: 'ListingPage', component: Listing },
+        { path: 'settings', name: 'SettingsPage', component: Settings },
       ]}
     ]
   }
@@ -56,7 +59,7 @@ router.beforeEach((to, from, next) => {
       router.push({name: to.name, params: { lang: 'en' }})
     }
   }else{
-    router.push({name: 'E404', params: { lang: 'en' }})
+    router.push({name: 'E404Page', params: { lang: 'en' }});
   }
 
   next()
