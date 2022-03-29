5e6389a0b2bdb7d61f1152ee51190f128bf26995
ArtemDutchak <artemdutchak@gmail.com>
2022-03-29 09:54:25 +0300

    add empty page component

---
+++ resources/js/components/pages/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <v-container>
+
+    <v-row>
+      <v-col class="text-center">
+        <span>
+          {{ title }}
+        </span>
+      </v-col>
+    </v-row>
+
+  </v-container>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Dashboard',
+
+  data: () => ({
+    title: "Dashboard",
+  }),
+
+  methods:{
+
+  },
+
+  computed:{
+
+  },
+
+  mounted() {
+
+  }
+
+}
+</script>
