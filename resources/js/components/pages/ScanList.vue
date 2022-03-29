c844a758e70201bcad02296c79baacfc69b562ff
ArtemDutchak <artemdutchak@gmail.com>
2022-03-29 09:54:52 +0300

    add empty page component

---
+++ resources/js/components/pages/ScanList.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <v-container>
+
+    <v-row>
+      <v-col class="text-center">
+        <span>
+          {{ title }}
+        </span>
+      </v-col>
+    </v-row>
+
+  </v-container>
+
+</template>
+
+<script>
+
+export default {
+  name: 'ScanList',
+
+  data: () => ({
+    title: "ScanList",
+  }),
+
+  methods:{
+
+  },
+
+  computed:{
+
+  },
+
+  mounted() {
+
+  }
+
+}
+</script>
