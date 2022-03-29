8d6c1aa526ab7ce1b951e19608df6f1106e4eb2d
ArtemDutchak <artemdutchak@gmail.com>
2022-03-29 09:54:21 +0300

    add empty page component

---
+++ resources/js/components/pages/CustomUrls.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <v-container>
+
+    <v-row>
+      <v-col class="text-center">
+        <span>
+          {{ title }}
+        </span>
+      </v-col>
+    </v-row>
+
+  </v-container>
+
+</template>
+
+<script>
+
+export default {
+  name: 'CustomUrls',
+
+  data: () => ({
+    title: "CustomUrls",
+  }),
+
+  methods:{
+
+  },
+
+  computed:{
+
+  },
+
+  mounted() {
+
+  }
+
+}
+</script>
